524372808eba2fe68c29f0adbc6b653953412ceb
Adam Jonas <francestown81@gmail.com>
2013-01-20 17:55:12 -0500

    more text edits

--- app.rb
+++ app.rb
@@ -1,6 +1,5 @@
 require 'sinatra'
 require 'koala'
-# require_relative 'env.rb'
 
 ### Configuration
 enable :sessions
@@ -65,17 +64,16 @@ get "/" do
 
   if session[:access_token]
     @user    = @graph.get_object("me")
-    @friends = @graph.get_connections('me', 'friends')
-    @photos  = @graph.get_connections('me', 'photos')
-    @likes   = @graph.get_connections('me', 'likes').first(4)
-
-    # for other data you can always run fql
-    @friends_using_app = @graph.fql_query("SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1")
+    # @friends = @graph.get_connections('me', 'friends')
+    # @photos  = @graph.get_connections('me', 'photos')
+    # @likes   = @graph.get_connections('me', 'likes').first(4)
+    
+    # # for other data you can always run fql
+    # @friends_using_app = @graph.fql_query("SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1")
   end
   erb :index
 end
 
-# used by Canvas apps - redirect the POST to be a regular GET
 post "/" do
   redirect "/"
 end
